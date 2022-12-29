1be365c4bcaaff3403520a0371c58fa362ac08fb
Daniel Liao <yazhengliao@gmail.com>
2022-12-29 15:59:53 -0500

    Added firebase and deployment script

---
+++ src/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDiaZ1ASsSGP6jjZjBUQXN0_pdJcB3XNao",
+    authDomain: "personal-website-8f505.firebaseapp.com",
+    databaseURL: "https://personal-website-8f505-default-rtdb.firebaseio.com",
+    projectId: "personal-website-8f505",
+    storageBucket: "personal-website-8f505.appspot.com",
+    messagingSenderId: "477534951771",
+    appId: "1:477534951771:web:6e158efe30eec3b203627e",
+    measurementId: "G-MZLVR47366",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
