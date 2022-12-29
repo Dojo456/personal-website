8a7cf37ef7320997fef91324acc892c6df56a759
Daniel Liao <yazhengliao@gmail.com>
2022-12-28 19:12:53 -0500

    Rescaling profile

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,61 +1,83 @@
-import { FC } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 const headshot = require("./Headshot.jpg");
 
-const BaseDiv = styled.div`
+const DisplayDiv = styled.div<{
+    overflow: boolean;
+}>`
     display: flex;
+    align-items: center;
     justify-content: center;
-`;
-
-const DisplayDiv = styled.div`
-    width: 70%;
-    height: 70%;
-    display: flex;
-    flex-direction: row;
-    background-color: slategrey;
-`;
-
-const ImageDiv = styled.div`
-    background-color: white;
+    width: 100%;
+    flex-direction: ${(props) => (props.overflow ? "column" : "row")};
 `;
 
 const HeadshotImg = styled.img`
     left: 0;
     margin: 30px;
     width: 400px;
-    height: auto;
+    height: 400px;
     border: medium solid black;
 `;
 
-const TextSpan = styled.div`
-    padding-left: 30px;
-    padding-right: 30px;
+const TextDiv = styled.div`
+    padding: 10px;
+`;
+
+const MainText = styled.h1`
+    text-transform: uppercase;
+    font-size: 60px;
+    margin-bottom: 0px;
+    margin-top: 0px;
+    min-width: 300px;
+    max-width: 100%;
+`;
+
+const HighlightedText = styled.code`
+    font-size: 120%;
+    color: white;
+`;
+
+const SubText = styled.p`
+    font-size: larger;
+    max-width: 650px;
+    margin-bottom: 0px;
+    margin-top: 0px;
 `;
 
 export const Profile: FC = () => {
+    const [overflow, setOverflow] = useState(false);
+    const textRef = useRef<HTMLHeadingElement>(null);
+
+    const handleResize = () => {
+        let newOverflow = false;
+
+        if (textRef !== null) {
+            if (textRef.current!.offsetWidth + 460 > window.innerWidth) {
+                newOverflow = true;
+            }
+        }
+
+        setOverflow(newOverflow);
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", handleResize, false);
+    });
+
     return (
-        <BaseDiv>
-            <DisplayDiv>
-                <ImageDiv>
-                    <HeadshotImg src={headshot} alt="headshot"></HeadshotImg>
-                </ImageDiv>
-                <TextSpan>
-                    <h1>Daniel Liao</h1>
-                    <p>
-                        This is a bio fffffffffffffffffffffffffffff
-                        ffffffffffffffffffffffffffsadf
-                        sdffffffffffffffffffffffffffffffff ffffffffffffffffff
-                        ffff f fffffffff ffffff ffffffffffffffffsadfsdfff
-                        fffffff ffffffffff ffffffffffffffff fffffffff
-                        fffffffffffffffffffffff ffffffffffffffffffsadfsd
-                        fffffffffffffffffff ffffffffffffff ffffffffffff
-                        ffffffffffffff fffffffff
-                        ffffffffffffffffffsadfsdfffffffffffffffffffffffffffffff
-                        ffffffffffffffff fffffffffffffffffff fffffffff
-                        fffffffffff
-                    </p>
-                </TextSpan>
-            </DisplayDiv>
-        </BaseDiv>
+        <DisplayDiv overflow={overflow}>
+            <TextDiv>
+                <MainText ref={textRef}>
+                    Hi. I'm <HighlightedText>Daniel</HighlightedText>
+                </MainText>
+                <SubText>
+                    I'm a student at{" "}
+                    <HighlightedText>Babson College</HighlightedText> studying
+                    Business Administration sdfsdfs sdfsfdds sdfsdf
+                </SubText>
+            </TextDiv>
+            <HeadshotImg src={headshot} alt="headshot"></HeadshotImg>
+        </DisplayDiv>
     );
 };
