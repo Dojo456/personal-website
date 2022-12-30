ce4f3f6ec4d271ed091141af691b126f9b06eebc
Daniel Liao <yazhengliao@gmail.com>
2022-12-29 23:01:54 -0500

    Basic github integration

--- src/timeline/Experience.tsx
+++ src/timeline/Experience.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
 enum ExperienceType {
     Work,
@@ -18,5 +18,17 @@ interface ExperienceDisplayProps {
 }
 
 export const ExperienceDisplay: FC<ExperienceDisplayProps> = (props) => {
+    const [experiences, setExperiences] = useState<Experience[]>([]);
+
+    useEffect(() => {
+        fetch("https://api.github.com/users/Dojo456/repos")
+            .then((resp) => {
+                resp.json().then((resp) => {
+                    console.log(resp);
+                });
+            })
+            .catch((err) => console.error(err));
+    }, []);
+
     return <>{JSON.stringify(props.experience)}</>;
 };
