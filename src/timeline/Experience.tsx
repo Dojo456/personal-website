047207caaa03330117fdb1bbf9732b1082d0a4a5
Daniel Liao <yazhengliao@gmail.com>
2023-01-05 17:24:42 -0500

    Basic selector for timeline

--- src/timeline/Experience.tsx
+++ src/timeline/Experience.tsx
@@ -1,22 +1,50 @@
 import { FC } from "react";
-
-enum ExperienceType {
-    Work,
-    Education,
-}
+import styled from "styled-components";
 
 export type Experience = {
-    type: ExperienceType;
-    org: string;
+    org?: string;
     title: string;
     startDate: Date;
     endDate: Date;
+    link?: string;
 };
 
 interface ExperienceDisplayProps {
     experience: Experience;
 }
 
+const MainDiv = styled.div`
+    width: 200px;
+    border: 1px black solid;
+    background-color: white;
+    margin: 10px;
+    height: min-content;
+    > * {
+        margin: 0px;
+    }
+`;
+
+function DateToString(date: Date) {
+    return new Intl.DateTimeFormat("en-US", {
+        month: "short",
+        year: "numeric",
+    }).format(date);
+}
+
 export const ExperienceDisplay: FC<ExperienceDisplayProps> = (props) => {
-    return <>{JSON.stringify(props.experience)}</>;
+    const experience = props.experience;
+
+    return (
+        <a href={experience.link}>
+            <MainDiv>
+                <h1>
+                    {DateToString(experience.startDate) +
+                        " - " +
+                        DateToString(experience.endDate)}
+                </h1>
+                <h2>{experience.title}</h2>
+                <p>{experience.org}</p>
+            </MainDiv>
+        </a>
+    );
 };
--- src/timeline/Timeline.tsx
+++ src/timeline/Timeline.tsx
@@ -6,50 +6,131 @@ import { Experience, ExperienceDisplay } from "./Experience";
 
 const DisplayDiv = styled.div`
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 50px;
     min-height: 100vh;
     width: 100%;
 `;
 
+const StyledLabel = styled.label``;
+
+enum ExperienceType {
+    Programming = "Programming",
+    Education = "Education",
+    Work = "Work",
+}
+
+type AllExperiences = {
+    [key in ExperienceType]: Experience[];
+};
+
 export const Timeline: FC = () => {
-    const [experiences, setExperiences] = useState<Experience[]>([]);
-
-    useEffect(() => {
-        getDocs(collection(firebase.firestore, "experiences"))
-            .then((querySnapshot) => {
-                const allExperiences = querySnapshot.docs.map(
-                    (snapshot) => snapshot.data() as Experience
-                );
-
-                allExperiences.sort(
-                    (a, b) => b.endDate.getTime() - a.endDate.getTime()
-                );
-
-                setExperiences(allExperiences);
-            })
-            .catch((reason) => console.error(reason));
-    }, []);
+    const [experiences, setExperiences] = useState<AllExperiences>();
+    const [selected, setSelected] = useState<ExperienceType>(
+        ExperienceType.Programming
+    );
 
     console.log(experiences);
 
+    const fetchAllExperiences = async () => {
+        const fetchGitHub = async () => {
+            const resp = await fetch(
+                "https://api.github.com/users/Dojo456/repos"
+            );
+            const data = await resp.json();
+
+            const repoExperiences: Experience[] = [];
+
+            for (const repo of data) {
+                repoExperiences.push(ParseRepo(repo));
+            }
+
+            return repoExperiences;
+        };
+
+        const fetchFirebase = async () => {
+            const querySnapshot = await getDocs(
+                collection(firebase.firestore, "experiences")
+            );
+            const allExperiences = querySnapshot.docs.map(
+                (snapshot) => snapshot.data() as Experience
+            );
+
+            allExperiences.sort(
+                (a, b) => b.endDate.getTime() - a.endDate.getTime()
+            );
+
+            return allExperiences;
+        };
+
+        return {
+            Programming: await fetchGitHub(),
+            Work: await fetchFirebase(),
+            Education: [],
+        };
+    };
+
     useEffect(() => {
-        fetch("https://api.github.com/users/Dojo456/repos")
-            .then((resp) => {
-                resp.json().then((resp) => {
-                    console.log(resp);
-                });
+        fetchAllExperiences()
+            .then((experiences) => {
+                setExperiences(experiences);
             })
             .catch((err) => console.error(err));
     }, []);
 
+    const onRadioButtonChange = (
+        event: React.SyntheticEvent<HTMLInputElement, Event>
+    ) => {
+        setSelected(
+            ExperienceType[
+                event.currentTarget.value as keyof typeof ExperienceType
+            ]
+        );
+    };
+
     return (
         <DisplayDiv>
-            {experiences.map((experience, idx) => (
-                <ExperienceDisplay
-                    key={idx}
-                    experience={experience}
-                ></ExperienceDisplay>
-            ))}
+            <form>
+                {(
+                    Object.keys(ExperienceType) as Array<
+                        keyof typeof ExperienceType
+                    >
+                ).map((enumLabel, idx) => {
+                    const key = enumLabel.valueOf();
+                    return (
+                        <StyledLabel htmlFor={key} key={idx * 2 + 1}>
+                            <input
+                                type="radio"
+                                name="experienceType"
+                                value={key}
+                                key={idx * 2}
+                                onChange={onRadioButtonChange}
+                                checked={key === selected}
+                            ></input>
+                            {key}
+                        </StyledLabel>
+                    );
+                })}
+            </form>
+            <div>
+                {experiences
+                    ? experiences[selected].map((experience, idx) => (
+                          <ExperienceDisplay
+                              key={idx}
+                              experience={experience}
+                          ></ExperienceDisplay>
+                      ))
+                    : undefined}
+            </div>
         </DisplayDiv>
     );
 };
+
+function ParseRepo(repo: any): Experience {
+    return {
+        title: repo.name,
+        startDate: new Date(repo.created_at),
+        endDate: new Date(repo.updated_at),
+    };
+}
