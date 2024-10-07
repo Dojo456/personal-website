c106cba4ef7a8ab9f0ec065702da0d0d89411678
Daniel Liao <yazhengliao@gmail.com>
2024-10-07 18:23:46 -0400

    Getting started

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
-from flask import Flask, render_template
+from dotenv import load_dotenv
+from flask import Flask, render_template, url_for
+from jinja2 import FileSystemLoader, select_autoescape
+
+load_dotenv()
 
 app = Flask(__name__)
+
+# Configure Jinja2 to recognize .jinja files
+
+app.jinja_env.loader = FileSystemLoader("templates")
+app.jinja_env.autoescape = select_autoescape(["html", "xml", "jinja"])
 
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.jinja")
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.jinja")
 
 
 @app.route("/projects")
 def projects():
-    return render_template("projects.html")
+    return render_template("projects.jinja")
+
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.jinja")
 
 
 if __name__ == "__main__":
--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,32 @@
 // You can add any JavaScript functionality here
 console.log("Welcome to my personal website!");
+
+const searchForm = document.getElementById("search-form");
+const searchInput = document.getElementById("query");
+const searchResults = document.getElementById("search-results");
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "Escape") {
+        searchForm.classList.add("hidden");
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.altKey && event.key === "p") {
+        searchForm.classList.remove("hidden");
+        searchInput.focus();
+    }
+});
+
+searchInput.addEventListener("input", function(event) {
+    const term = event.target.value;
+
+    console.log(term)
+
+    if (term === "meowmeow") {
+        searchResults.innerHTML = "You found me!";
+    } else {
+        searchResults.innerHTML = ""
+    }
+
+});
